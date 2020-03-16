29b86afa26582aa69b23f0e8f8f3e6a760826eb1
els <els@LAPTOP-7IIQL0MB>
2020-03-16 16:04:47 +0700

    The Super Computer

---
+++ TheSuperComputer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheSuperComputer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            monitor lgMonitor = new monitor("1200x600", true, 32);
+            printer hpPrinter = new printer("HP", "MP102", 50);
+
+            vga nvdiavga = new vga("Nvdia", 2054, 2054);
+            Ram samsungRam = new Ram("DDR4", 5000, 8000);
+            Processor intelProcessor = new Processor();
+            intelProcessor.setCache(254);
+            intelProcessor.setCore(8);
+            intelProcessor.setSeries("CORE i7 8th Gen");
+
+            computer computer = new computer.Builder(samsungRam, nvdiavga, intelProcessor)
+                .withMonitor(lgMonitor)
+                .withPrinter(hpPrinter)
+                .builder();
+            Console.WriteLine(computer.ToString());
+        }
+    }
+}
+      
---
+++ TheSuperComputer/computer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSuperComputer
+{
+    class computer
+    {
+        private monitor monitor;
+        private printer printer;
+        private Processor processor;
+        private Ram ram;
+        private vga vga;
+
+        private computer(Builder builder)
+        {
+            this.monitor = builder.monitor;
+            this.processor = builder.processor;
+            this.printer = builder.printer;
+            this.ram = builder.ram;
+            this.vga = builder.vga;
+        }
+        public override string ToString()
+        {
+            return $"Monitor spec : {this.monitor} \n" +
+              $"Processor spec : {this.processor} \n" +
+              $"Printer spec : {this.printer} \n" +
+              $"Vga spec : {this.vga} \n" +
+              $"Ram spec : {this.ram}";
+
+        }
+        public class Builder
+        {
+            public monitor monitor;
+            public printer printer;
+            public Processor processor;
+            public Ram ram;
+            public vga vga;
+
+            public Builder(Ram ram, vga vga, Processor processor)
+            {
+                this.ram = ram;
+                this.vga = vga;
+                this.processor = processor;
+            }
+
+            public Builder withMonitor(monitor monitor)
+            {
+                this.monitor = monitor;
+                return this;
+            }
+
+            public Builder withPrinter(printer printer)
+            {
+                this.printer = printer;
+                return this;
+            }
+
+            public computer builder()
+            {
+                return new computer(this);
+            }
+        }
+    }
+}
+  
